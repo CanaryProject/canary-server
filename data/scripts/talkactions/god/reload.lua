1e1057e58ff7d3dd62582a316a3ea72760f9badf
eduardodantas <eduardo.dantas@hotmail.com.br>
2020-06-20 02:32:36 -0300

    Fixes on the reload talkaction
    Removed reload types that no longer exist
    Fixed error in function logCommand

--- data/scripts/talkactions/god/reload.lua
+++ data/scripts/talkactions/god/reload.lua
@@ -1,9 +1,6 @@
 local reloadTypes = {
 	["all"] = RELOAD_TYPE_ALL,
 
-	["action"] = RELOAD_TYPE_ACTIONS,
-	["actions"] = RELOAD_TYPE_ACTIONS,
-
 	["chat"] = RELOAD_TYPE_CHAT,
 	["channel"] = RELOAD_TYPE_CHAT,
 	["chatchannels"] = RELOAD_TYPE_CHAT,
@@ -11,15 +8,9 @@ local reloadTypes = {
 	["config"] = RELOAD_TYPE_CONFIG,
 	["configuration"] = RELOAD_TYPE_CONFIG,
 
-	["creaturescript"] = RELOAD_TYPE_CREATURESCRIPTS,
-	["creaturescripts"] = RELOAD_TYPE_CREATURESCRIPTS,
-
 	["events"] = RELOAD_TYPE_EVENTS,
 
 	["global"] = RELOAD_TYPE_GLOBAL,
-
-	["globalevent"] = RELOAD_TYPE_GLOBALEVENTS,
-	["globalevents"] = RELOAD_TYPE_GLOBALEVENTS,
 
 	["items"] = RELOAD_TYPE_ITEMS,
 
@@ -32,30 +23,11 @@ local reloadTypes = {
 	["mount"] = RELOAD_TYPE_MOUNTS,
 	["mounts"] = RELOAD_TYPE_MOUNTS,
 
-	["move"] = RELOAD_TYPE_MOVEMENTS,
-	["movement"] = RELOAD_TYPE_MOVEMENTS,
-	["movements"] = RELOAD_TYPE_MOVEMENTS,
-
 	["npc"] = RELOAD_TYPE_NPCS,
 	["npcs"] = RELOAD_TYPE_NPCS,
 
 	["raid"] = RELOAD_TYPE_RAIDS,
 	["raids"] = RELOAD_TYPE_RAIDS,
-
-	["spell"] = RELOAD_TYPE_SPELLS,
-	["spells"] =  RELOAD_TYPE_SPELLS,
-
-	["stages"] = RELOAD_TYPE_STAGES,
-	["stage"] = RELOAD_TYPE_STAGES,
-	["rate"] = RELOAD_TYPE_STAGES,
-	["rates"] = RELOAD_TYPE_STAGES,
-
-	["talk"] = RELOAD_TYPE_TALKACTIONS,
-	["talkaction"] = RELOAD_TYPE_TALKACTIONS,
-	["talkactions"] = RELOAD_TYPE_TALKACTIONS,
-
-	["weapon"] = RELOAD_TYPE_WEAPONS,
-	["weapons"] = RELOAD_TYPE_WEAPONS,
 
 	["scripts"] = RELOAD_TYPE_SCRIPTS,
 	["libs"] = RELOAD_TYPE_GLOBAL
@@ -79,11 +51,11 @@ function reload.onSay(player, words, param)
 	if reloadType then
 		Game.reload(reloadType)
 		player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Reloaded %s.", param:lower()))
-		print("Reloaded: " .. param:lower() .. ".")
+		print("Reloaded " .. param:lower() .. "")
 		return true
 	elseif not reloadType then
 		player:sendCancelMessage("Reload type not found.")
-		print("Reload type not found.")
+		print("Reload type not found")
 		return false
 	end
 	return false
