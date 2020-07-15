8fb2920f3f6a0dabc101489a79da4f5d55530aee
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-07-15 16:41:19 -0300

    [BashScripts]: improve bash scripts flags and add some color.

--- build.sh
+++ build.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/env bash
 TYPE="Debug"
 TESTS="Off"
-EXECUTE="On"
+EXECUTE="Off"
+
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+CANARY_PROJECT="${ORANGE}[CanaryProject]${NC}"
 
 usage()
 {
-    echo "usage: ./build.sh [[-r Debug/Release] [-t On/Off] [-e Off]]"
+    echo -e "${CANARY_PROJECT} Usage: -r for release, -t for build tests and -e for execute]]"
+    exit 1
 }
 
-while getopts ":r:t:e:" opt; do
+while getopts "rteh" opt; do
   case "$opt" in
-    r) 
-      TYPE=$OPTARG 
-      if [ "$TYPE" != "Debug" -a "$TYPE" != "Release" ]; then
-        usage
-        exit 1
-      fi
-      ;;
-    t) TESTS=$OPTARG 
-      if [ "$TESTS" != "On" -a "$TESTS" != "Off" ]; then
-        usage
-        exit 1
-      fi
-    ;;
-    e) EXECUTE=$OPTARG;;
+    r) TYPE="Release" ;;
+    t) TESTS="On" ;;
+    e) EXECUTE="On" ;;
+    h) usage ;;
   esac
 done
 
@@ -33,6 +28,6 @@ cd ..
 rm -rf canary
 cp build/bin/canary ./
 
-if [ "$EXECUTE" != "Off" ]; then
+if [ "$EXECUTE" = "On" ]; then
   ./canary
 fi
--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,42 @@
 #!/usr/bin/env bash
 
-echo "Running server tests"
-./build/bin/canary-tests --reporter compact --success -d yes
+TESTS="ALL"
 
-echo "Running lib tests"
-./canary-lib/tests/canary-lib-tests --reporter compact --success -d yes
+ORANGE='\033[0;33m'
+NC='\033[0m' # No Color
+CANARY_PROJECT="${ORANGE}[CanaryProject]${NC}"
+
+usage()
+{
+    echo -e "${CANARY_PROJECT} Usage: [--all, --server, --lib]"
+    echo -e "${CANARY_PROJECT} You can pass tests arguments, like -s, --help or -d yes"
+    exit 1
+}
+
+while getopts ":-:h" opt; do
+  case ${opt} in
+    -) 
+      case ${OPTARG} in
+        "all"*) TESTS="ALL"
+            ;;
+        "lib"*) TESTS="LIB"
+            ;;
+        "server"*) TESTS="SERVER"
+            ;;
+      esac 
+    ;;
+    h) usage ;;
+  esac
+done
+
+# useful arguments --success -d yes
+
+if [ ${TESTS} != "LIB" ]; then
+  echo -e "${CANARY_PROJECT} Running server tests"
+  ./build/bin/canary-tests $@
+fi
+
+if [ ${TESTS} != "SERVER" ]; then
+  echo -e "${CANARY_PROJECT} Running lib tests"
+  ./canary-lib/tests/canary-lib-tests $@
+fi
