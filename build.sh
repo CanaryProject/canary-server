3e59712c97ea9b9644320719f1d6acebfd782ef5
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-07-18 17:11:22 -0300

    [BuildScript]: add verbose and silent.

--- build.sh
+++ build.sh
@@ -3,26 +3,27 @@ TYPE="Debug"
 TESTS="OFF"
 EXECUTE="OFF"
 WARNING_FLAGS="OFF"
-REMOVE_EXTERNAL_WARNINGS="ON"
+OPTIMIZATIONS="ON"
 
 usage()
 {
-    ./canary_echo.sh "Usage: -e for execute, -r for release, -t for build tests and -v for verbose]]"
+    ./canary_echo.sh "Usage: -e for execute, -r for release, -s for silent, -t for build tests and -v for verbose]]"
     exit 1
 }
 
-while getopts "ehrtv" opt; do
+while getopts "ehrstv" opt; do
   case "$opt" in
     e) EXECUTE="ON" ;;
     h) usage ;;
     r) TYPE="Release" ;;
+    s) WARNING_FLAGS="OFF" OPTIMIZATIONS="OFF" ;;
     t) TESTS="ON" ;;
-    v) WARNING_FLAGS="ON" REMOVE_EXTERNAL_WARNINGS="OFF" ;;
+    v) WARNING_FLAGS="ON" OPTIMIZATIONS="ON" ;;
   esac
 done
 
 cd build
-cmake -DOPTIONS_ENABLE_UNIT_TEST=${TESTS} -DCMAKE_BUILD_TYPE=${TYPE} -DOPTIONS_ENABLE_IPO=${REMOVE_EXTERNAL_WARNINGS} -DOPTIONS_WARNINGS_FLAGS=${WARNING_FLAGS} .. ; make -j`nproc`
+cmake -DOPTIONS_ENABLE_UNIT_TEST=${TESTS} -DCMAKE_BUILD_TYPE=${TYPE} -DOPTIONS_ENABLE_IPO=${OPTIMIZATIONS} -DOPTIONS_WARNINGS_FLAGS=${WARNING_FLAGS} .. ; make -j`nproc`
 cd ..
 rm -rf canary
 cp build/bin/canary ./
