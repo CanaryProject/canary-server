635b064287f7d84afbec9870547937fabd41b0e8
Lucas Grossi <lucas.ggrossi@gmail.com>
2020-07-18 02:13:07 -0300

    [BuildScript]: add verbose flag.

--- build.sh
+++ build.sh
@@ -2,24 +2,27 @@
 TYPE="Debug"
 TESTS="Off"
 EXECUTE="Off"
+WARNING_FLAGS="Off"
+REMOVE_EXTERNAL_WARNINGS="On"
 
 usage()
 {
-    ./canary_echo.sh "Usage: -r for release, -t for build tests and -e for execute]]"
+    ./canary_echo.sh "Usage: -e for execute, -r for release, -t for build tests and -v for verbose]]"
     exit 1
 }
 
-while getopts "rteh" opt; do
+while getopts "ehrtv" opt; do
   case "$opt" in
-    r) TYPE="Release" ;;
-    t) TESTS="On" ;;
     e) EXECUTE="On" ;;
     h) usage ;;
+    r) TYPE="Release" ;;
+    t) TESTS="On" ;;
+    v) WARNING_FLAGS="On" REMOVE_EXTERNAL_WARNINGS="Off" ;;
   esac
 done
 
 cd build
-sudo cmake -DOPTIONS_ENABLE_UNIT_TEST=${TESTS} -DCMAKE_BUILD_TYPE=${TYPE} .. ; make
+sudo cmake -DOPTIONS_ENABLE_UNIT_TEST=${TESTS} -DOPTIONS_ENABLE_IPO={REMOVE_EXTERNAL_WARNINGS} -DOPTIONS_WARNINGS_FLAGS={WARNING_FLAGS} -DCMAKE_BUILD_TYPE=${TYPE} .. ; make
 cd ..
 rm -rf canary
 cp build/bin/canary ./
